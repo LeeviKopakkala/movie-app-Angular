613adfceb5ae0c437812dbbdbcddcd9367fd70ec
Leevi Kopakkala <leevi.kopakkala@hotmail.com>
2018-11-16 17:54:58 +0200

    Create base + Add SASS support

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+const routes: Routes = [
+    {path: '', component: HomeComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'movie-app';
+export class AppComponent implements OnInit {
+  showMenu = false;
+
+  ngOnInit() {
+
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+
 }
