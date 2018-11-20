6628cd0790432172c6eeaffb3be1d8c5cdc36e65
Leevi Kopakkala <leevi.kopakkala@hotmail.com>
2018-11-20 04:09:27 +0200

    Add search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from  '../../services/api.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -11,13 +13,26 @@ export class SearchComponent implements OnInit {
 
   keyword: string;
   private sub: any;
+  searchForm: FormGroup;
 
-  constructor(private  apiService:  ApiService, private route: ActivatedRoute) { }
+  constructor(private  apiService:  ApiService, private route: ActivatedRoute, private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.keyword = params['keyword']; // (+) converts string 'id' to a number
    });
+   this.searchForm = this.formBuilder.group({
+    search: ['', Validators.required]
+  });
+  }
+
+  // Get form fields
+  get f() { return this.searchForm.controls; }
+
+  public onSearch(){
+    
+    this.router.navigate(['/search/'+this.f.search.value]);
+    window.location.reload();
   }
 
 }
